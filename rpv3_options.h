556ff4e301d5234a36702dd2ab7e9b7c2910ed1f
Mike Vermeulen <mike@vermeulen.com>
2025-11-27 21:18:44 -0600

    update tests

--- rpv3_options.h
+++ rpv3_options.h
@@ -11,10 +11,10 @@ extern "C" {
 #endif
 
 /* Version information */
-#define RPV3_VERSION "1.4.4"
+#define RPV3_VERSION "1.4.5"
 #define RPV3_VERSION_MAJOR 1
 #define RPV3_VERSION_MINOR 4
-#define RPV3_VERSION_PATCH 4
+#define RPV3_VERSION_PATCH 5
 
 
 /* Return codes */
